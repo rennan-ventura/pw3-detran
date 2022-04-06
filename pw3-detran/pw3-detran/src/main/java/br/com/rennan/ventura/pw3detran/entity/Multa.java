d365ac32da174cc8211eb62d94729d6196914101
Rennan-Iann <Rennan-Iann@teste.com>
2022-04-06 14:29:16 -0300

    Criacao da entidade Multa

---
+++ pw3-detran/pw3-detran/src/main/java/br/com/rennan/ventura/pw3detran/entity/Multa.java
@@ -0,0 +1,50 @@
+package br.com.rennan.ventura.pw3detran.entity;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import br.com.rennan.ventura.pw3detran.enums.EstadoMultaEnum;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@Entity
+@Table(name = "multa")
+public class Multa {
+	
+	@Id
+	@Column(name = "idMulta")
+	private int id;
+	
+	@Column(name = "dataAplicacao")
+	private Date aplicacao;
+	
+	@Column(name = "vencimento")
+	private Date vencimento;
+	
+	@Column(name = "estadoMulta")
+	@Enumerated(EnumType.STRING)
+	private EstadoMultaEnum estadoMulta;
+	
+	@Column(name = "orgao")
+	private String orgao;
+	
+	@ManyToOne
+	@JoinColumn(name = "idVeiculo")
+	private Veiculo veiculo;
+	
+	@ManyToOne
+	@JoinColumn(name = "idPessoa")
+	private Pessoa condutor;
+
+}
