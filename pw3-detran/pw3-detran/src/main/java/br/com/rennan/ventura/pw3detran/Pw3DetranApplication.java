bc334635274c9d195adccabff88083ebc247d913
Rennan-Iann <Rennan-Iann@teste.com>
2022-04-06 13:32:53 -0300

    Criacao do projeto

---
+++ pw3-detran/pw3-detran/src/main/java/br/com/rennan/ventura/pw3detran/Pw3DetranApplication.java
@@ -0,0 +1,13 @@
+package br.com.rennan.ventura.pw3detran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pw3DetranApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pw3DetranApplication.class, args);
+	}
+
+}
---
+++ pw3-detran/pw3-detran/src/test/java/br/com/rennan/ventura/pw3detran/Pw3DetranApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rennan.ventura.pw3detran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pw3DetranApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
