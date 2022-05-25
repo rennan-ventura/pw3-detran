7baa8e803f862d9a67f4270e9cea85b8f4beec27
pedro <rennan@email.com>
2022-05-25 14:38:55 -0300

    criando status de retorno

--- pw3-detran/pw3-detran/src/main/java/br/com/rennan/ventura/pw3detran/controller/PessoaController.java
+++ pw3-detran/pw3-detran/src/main/java/br/com/rennan/ventura/pw3detran/controller/PessoaController.java
@@ -2,9 +2,11 @@ package br.com.rennan.ventura.pw3detran.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,39 +29,47 @@ public class PessoaController {
 	List<Pessoa> pessoas = new ArrayList<Pessoa>();
 	
 	@GetMapping
-	public List<Pessoa> getAll(){
+	//@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<List<Pessoa>> getAll(){
 		pessoas = repository.findAll();
-		return pessoas;
+		return ResponseEntity.ok(pessoas);
 	}
 	
 	@GetMapping("/{id}")
-	public Pessoa getById(@PathVariable Integer id){
-		Pessoa tipo = new Pessoa();
-		tipo = repository.findById(id).get();
-		return tipo;
+	//@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<Pessoa> getById(@PathVariable Integer id){
+		Optional<Pessoa> tipo = repository.findById(id);
+		if (tipo.isPresent()) {
+			ResponseEntity.noContent().build();
+		}
+		return ResponseEntity.ok(tipo.get());
 		
 	}
 		
 	@PostMapping	
-	@ResponseStatus(HttpStatus.CREATED)
-	public void inserir (@RequestBody Pessoa pessoa) {
+	//@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Pessoa> inserir (@RequestBody Pessoa pessoa) {
 		repository.save(pessoa);
+		return ResponseEntity.status((HttpStatus.CREATED)).body(pessoa);
 	}
 	
 	@PutMapping("/{id}")
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void atualizar(@PathVariable Integer id, @RequestBody Pessoa pessoa) {
+	//@ResponseStatus(HttpStatus.NO_CONTENT)
+	public ResponseEntity<Pessoa> atualizar(@PathVariable Integer id, @RequestBody Pessoa pessoa) {
 		
 		boolean existe = repository.existsById(id);
 		if(existe) {
 			repository.save(pessoa);
+			return ResponseEntity.status(HttpStatus.CREATED).body(pessoa);
 		}
+		return ResponseEntity.noContent().build();
 	}
 	
 	@DeleteMapping("/{id}")
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void deletar(@PathVariable Integer id) {
+	//@ResponseStatus(HttpStatus.NO_CONTENT)
+	public ResponseEntity<Pessoa> deletar(@PathVariable Integer id) {
 		repository.deleteById(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
